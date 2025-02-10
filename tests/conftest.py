ae9f89dd11e1f84197e8f754f24d81f78427d62a
Darya Nesknonnaya <feoktistova.dariafeok@yandex.ru>
2025-02-10 12:32:20 +0300

    Добавлена зависимость

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ from selene import browser
 from selenium import webdriver
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(autouse=True, scope='session')
 def browser_management():
     browser.config.timeout = 2.0
     browser.config.window_width = 1920
