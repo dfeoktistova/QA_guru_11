5e759edbc23ef8f91e7626e7e0c6b144aa83a94b
Darya Nesknonnaya <feoktistova.dariafeok@yandex.ru>
2025-02-10 12:33:22 +0300

    Отладка

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,9 +9,7 @@ def browser_management():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
 
-    driver_options = webdriver.ChromeOptions()
-    driver_options.page_load_strategy = 'eager'
-    browser.config.driver_options = driver_options
+    webdriver.ChromeOptions()
 
     yield
 
