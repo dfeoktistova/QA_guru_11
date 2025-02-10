23530f878e6e2ec1c54a7e56c9cc4dd01288b027
Darya Nesknonnaya <feoktistova.dariafeok@yandex.ru>
2025-02-10 13:23:33 +0300

    Добавлен конфиг для селенойда

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,35 @@
 import pytest
-from selene import browser
+from selene import Browser, Config
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(autouse=True, scope='session')
 def browser_management():
-    browser.config.timeout = 2.0
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+
+    browser = Browser(Config(driver=driver))
+
+    '''browser.config.timeout = 2.0
     browser.config.window_width = 1920
     browser.config.window_height = 1080
 
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
-    browser.config.driver_options = driver_options
+    browser.config.driver_options = driver_options'''
 
-    yield
+    yield browser
 
     browser.quit()
