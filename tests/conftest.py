2518c6aeab5e94fad5acd70f6b4f31c31768cafa
Darya Nesknonnaya <feoktistova.dariafeok@yandex.ru>
2025-02-10 13:46:05 +0300

    Отладка

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ import pytest
 from selene import Browser, Config
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import os
 
 
 @pytest.fixture(autouse=True, scope='session')
@@ -18,24 +19,30 @@ def browser_management():
         "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
-            "enableVideo": False
+            "enableVideo": True
         }
     }
 
     options.capabilities.update(selenoid_capabilities)
+
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
+
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options)
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+        options=options,
+        keep_alive=True)
 
     browser = Browser(Config(driver=driver))
 
-    '''browser.config.timeout = 2.0
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
-    browser.config.driver_options = driver_options'''
+    browser.config.driver_options = driver_options
+
+    browser.config.timeout = 2.0
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
 
     yield browser
 
