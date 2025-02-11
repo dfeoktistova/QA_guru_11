ab225226d9f4a72bcf681de04586680c5b74ea38
Darya Nesknonnaya <feoktistova.dariafeok@yandex.ru>
2025-02-11 12:04:42 +0300

    Изменена версия хрома

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,7 @@ def browser_management():
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "126.0",
+        "browserVersion": "120.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
