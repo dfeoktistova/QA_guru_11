bc92e6eb91a2699570d87348ac92eb06465b1872
Darya Nesknonnaya <feoktistova.dariafeok@yandex.ru>
2025-02-11 11:51:12 +0300

    Отладка

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,8 @@
-from dotenv import load_dotenv
 import pytest
-from selene import Browser, Config
+from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import os
-
-
-@pytest.fixture(autouse=True, scope='session')
-def load_venv():
-    load_dotenv()
+from utils import attach
 
 
 @pytest.fixture(autouse=True, scope='function')
@@ -24,17 +18,11 @@ def browser_management():
     }
 
     options.capabilities.update(selenoid_capabilities)
-
-    selenoid_login = os.getenv("SELENOID_LOGIN")
-    selenoid_pass = os.getenv("SELENOID_PASS")
-    selenoid_url = os.getenv("SELENOID_URL")
-
     driver = webdriver.Remote(
-        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
-        options=options,
-        keep_alive=True)
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
 
-    browser = Browser(Config(driver=driver))
+    browser.config.driver = driver
 
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
@@ -45,5 +33,10 @@ def browser_management():
     browser.config.window_height = 1080
 
     yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
--- tests/test_fill_form.py
+++ tests/test_fill_form.py
@@ -1,4 +1,5 @@
 from model.registration_page import RegistrationPage
+import allure
 
 first_name = 'Santa'
 last_name = 'Claus'
@@ -16,27 +17,53 @@ state = 'NCR'
 city = 'Noida'
 
 
-def test_fill_form():
+def test_fill_form(browser_set):
     registration_page = RegistrationPage()
 
-    registration_page.open()
+    with allure.step("Открыть страницу регистрации"):
+        registration_page.open()
 
-    registration_page.fill_first_name(first_name)
-    registration_page.fill_last_name(last_name)
-    registration_page.fill_email(email)
-    registration_page.select_gender(gender)
-    registration_page.fill_user_number(user_number)
-    registration_page.select_date_of_birth(birth_month, birth_year)
-    registration_page.fill_subjects(subjects)
-    registration_page.select_hobbies(hobby)
-    registration_page.upload_picture(picture)
-    registration_page.fill_address(address)
-    registration_page.select_state(state)
-    registration_page.select_city(city)
+    with allure.step("Заполнить поле 'first_name'"):
+        registration_page.fill_first_name(first_name)
 
-    registration_page.submit()
+    with allure.step("Заполнить поле 'last_name'"):
+        registration_page.fill_last_name(last_name)
 
-    registration_page.should_have_registered(first_name, last_name, email, gender, user_number, birth_day, birth_month,
-                                             birth_year, subjects, picture, address, state, city)
+    with allure.step("Заполнить поле 'email'"):
+        registration_page.fill_email(email)
+
+    with allure.step("Заполнить поле 'gender'"):
+        registration_page.select_gender(gender)
+
+    with allure.step("Заполнить поле 'user_number'"):
+        registration_page.fill_user_number(user_number)
+
+    with allure.step("Заполнить поля 'birth_month' и 'birth_year'"):
+        registration_page.select_date_of_birth(birth_month, birth_year)
+
+    with allure.step("Заполнить поле 'subjects'"):
+        registration_page.fill_subjects(subjects)
+
+    with allure.step("Заполнить поле 'hobby'"):
+        registration_page.select_hobbies(hobby)
+
+    with allure.step("Заполнить поле 'picture'"):
+        registration_page.upload_picture(picture)
+
+    with allure.step("Заполнить поле 'address'"):
+        registration_page.fill_address(address)
+
+    with allure.step("Заполнить поле 'state'"):
+        registration_page.select_state(state)
+
+    with allure.step("Заполнить поле 'city'"):
+        registration_page.select_city(city)
+
+    with allure.step("Подтвердить регистрацию"):
+        registration_page.submit()
+
+    with allure.step("Сравнить результаты"):
+        registration_page.should_have_registered(first_name, last_name, email, gender, user_number, birth_day, birth_month,
+                                                 birth_year, subjects, picture, address, state, city)
 
 
